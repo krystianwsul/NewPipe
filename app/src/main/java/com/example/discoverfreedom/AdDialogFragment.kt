721bac727af71231048a69522f2a0e2752b9a5a1
krystianwsul <krystianwsul@gmail.com>
2019-11-14 17:20:00 +0100

    AdDialogFragment: open Firefox app listing

--- app/src/main/java/com/example/discoverfreedom/AdDialogFragment.kt
+++ app/src/main/java/com/example/discoverfreedom/AdDialogFragment.kt
@@ -1,5 +1,7 @@
 package com.example.discoverfreedom
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Html
 import androidx.appcompat.app.AlertDialog
@@ -10,6 +12,8 @@ class AdDialogFragment : DialogFragment() {
     companion object {
 
         fun newInstance() = AdDialogFragment()
+
+        private const val FREE_DOM_PACKAGE = "org.mozilla.firefox"
     }
 
     @Suppress("DEPRECATION")
@@ -17,6 +21,12 @@ class AdDialogFragment : DialogFragment() {
             .setTitle(R.string.app_name)
             .setMessage(Html.fromHtml("<b>Remove all disruptive ads with Free.dom App.</b><br><br>Download Free.dom app and enjoy your experience without any ads."))
             .setNegativeButton("No, thanks") { _, _ -> }
-            .setPositiveButton("Get App") { _, _ -> }
+            .setPositiveButton("Get App") { _, _ ->
+                try {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$FREE_DOM_PACKAGE")))
+                } catch (exception: android.content.ActivityNotFoundException) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$FREE_DOM_PACKAGE")))
+                }
+            }
             .create()
 }
